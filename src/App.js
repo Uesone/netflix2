a07bc7256af1b7fed957282125ba130fdd9b5b97
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-06-28 16:14:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,47 @@ function App() {
   return (
     <div className="container-fluid px-4">
       <Navbar />
+
+      <div className="d-flex justify-content-between">
+        <div className="d-flex">
+          <h2 className="mb-4">TV Shows</h2>
+          <div className="btn-group" role="group">
+            <div className="dropdown ms-4 mt-1">
+              <button
+                type="button"
+                className="btn btn-secondary btn-sm dropdown-toggle rounded-0"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                style={{ backgroundColor: "#221f1f" }}
+              >
+                Genres
+              </button>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Comedy
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Drama
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Thriller
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div>
+          <i className="bi bi-grid icons"></i>
+          <i className="bi bi-grid-3x3 icons"></i>
+        </div>
+      </div>
+
       <Gallery title="Harry Potter" />
       <Gallery title="Lord of the Rings" />
       <Gallery title="Star Wars" />
